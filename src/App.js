4921a3d91da2c9a8cf2b68b6a80d6b12330023e6
kingggtw0 <halong669@gmail.com>
2025-01-14 22:32:27 +0700

    fix build

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
-import React, { useReducer, useRef } from "react";
+import React, { useRef } from "react";
 
 import "./App.css";
-
-import { InputText } from "primereact/inputtext";
 import ChartBar from "./components/CharBar";
-
-const reducer = (state, { type, data }) => {
-  switch (type) {
-    case "update":
-      let { rowIndex, rowData } = data ?? {};
-      state.results[rowIndex] = rowData;
-      return { ...state };
-    case "init":
-      return { ...state, results: data };
-    default:
-      throw new Error();
-  }
-};
-
-const defaultData = {
-  results: [
-    { id: 1, name: "Jone", address: "New York" },
-    { id: 2, name: "Maya", address: "New York" },
-  ],
-};
-
-const init = (initialState) => initialState;
-
-const textEditor = (options) => {
-  return (
-    <InputText
-      type="text"
-      value={options.value}
-      onChange={(e) => options.editorCallback(e.target.value)}
-      onBlur={(e) => {
-        const { props, rowData, rowIndex } = options;
-        const { onRowEditComplete, value: dataTable } = props;
-        dataTable[rowIndex]["loading"] = true;
-        onRowEditComplete({ rowData, rowIndex });
-      }}
-      onKeyDown={(e) => e.stopPropagation()}
-    />
-  );
-};
 
 const App = () => {
   const refChart = useRef(null);
@@ -79,15 +38,13 @@ const App = () => {
           if (refChart.current) {
             refChart.current.options.scales.x.min = 0;
             refChart.current.options.scales.x.max = 60;
-            refChart.current.update()
+            refChart.current.update();
           }
         }}
       >
         reset
       </button>
-      <ChartBar
-        ref={refChart}
-      />
+      <ChartBar ref={refChart} />
     </div>
   );
 };
